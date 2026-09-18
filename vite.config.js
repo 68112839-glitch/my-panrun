8c73f70451f88c0da26d64f6315d32f944448a30
Your Name <you@example.com>
2026-09-18 11:17:02 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'my-panrun/',
+  base: '/my-panrun/',
   plugins: [react(),
   VitePWA({
     registerType: 'autoUpdate',
